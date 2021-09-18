7ccbad99c907ad7556383504dc5ecb18258aa509
farmobus <69415206+farmobus@users.noreply.github.com>
2021-09-18 14:53:53 +0300

    Add files via upload

---
+++ scripts/send_message.js
@@ -0,0 +1,21 @@
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function delay() {
+    await sleep(1000);
+    answer();
+}
+
+function send() {
+    var dialog = document.querySelector("div.dialog");
+    var empty_dialog = document.getElementById("empty-dialog");
+    var input_message = document.getElementById("input_message").value;
+    var new_message = document.createElement("div");
+    new_message.innerHTML = input_message;
+    new_message.className = 'new_message';
+    empty_dialog.innerHTML = '';
+    dialog.appendChild(new_message);
+    sleep(2000)
+    delay(); 
+}
